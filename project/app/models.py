3e72d18b4bc5c5d1a562fad6259d2921a398aa76
AzarovDN <azarovdn@icloud.com>
2019-06-10 14:38:58 +1000

    Доделал вторую задачу. Криво выводит данные на карту.

--- project/app/models.py
+++ project/app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Route(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return self.name
+
 
 class Station(models.Model):
     latitude = models.FloatField()
--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Route
+
+
+def stations(request):
+
+    route = request.GET.get('route')
+    routes = Route.objects.all()
+
+    context = {'routes': routes}
+
+    if route:
+        route_obj = Route.objects.get(name=route)
+        stations = route_obj.stations.all()
+
+        x = (stations.first().latitude + stations.last().latitude)/2
+        y = (stations.first().longitude + stations.last().longitude)/2
+        center = {
+            'x': x,
+            'y': y
+        }
+
+        context['stations'] = stations
+        context['center'] = center
+
+    return render(request, 'stations.html', context)
